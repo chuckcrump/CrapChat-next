31fef8c1ba44fab42a15404b1a7043e5dfee3552
Andy <andothomas167@gmail.com>
2025-04-18 19:29:13 -0700

    Remove some stuff

--- server/controllers/zodSchemas.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const chatsSchema = z.object({
-  id: z.string().uuid().optional(),
-  role: z.string().min(1),
-  content: z.string().nullable().optional(),
-  user: z.boolean(),
-});
